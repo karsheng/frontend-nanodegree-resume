704b71b29682cda0ae1886e0d1c2d85d5cbe04f5
Kar Sheng Lee <karshenglee@Kars-MacBook-Air.local>
2016-07-07 16:03:38 +0800

    added education section

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -45,3 +45,75 @@ var bio = {
 };
 
 bio.display();
+
+var education = {
+  'schools' : [
+    {
+      'name' : 'University of Nottingham',
+      'location' : 'Nottingham, UK',
+      'degree' : 'MSc',
+      'majors' : 'Chemical Engineering',
+      'dates' : '2011-2012',
+      'url' : 'https://www.nottingham.ac.uk/'
+    },
+    {
+      'name' : 'University of Nottingham',
+      'location' : 'Kuala Lumpur, Malaysia',
+      'degree' : 'BEng',
+      'majors' : 'Chemical Engineering',
+      'dates' : '2008-2011',
+      'url' : 'https://www.nottingham.ac.uk/'
+    }
+  ],
+  'onlineCourses' : [
+    {
+      'title' : 'Front-End Web Developer Nanodegree',
+      'school' : 'Udacity',
+      'dates' : '2016 - ongoing',
+      'url' : 'https://www.udacity.com'
+    },
+    {
+      'title' : 'The Complete Web Development Course',
+      'school' : 'Udemy',
+      'dates' : '2015',
+      'url' : 'https://www.udemy.com'
+    }
+  ],
+  'display' : function() {
+    for (i = 0; i < education.schools.length; i++) {
+      $('#education').append(HTMLschoolStart);
+
+      var formattedSchoolName = HTMLschoolName.replace('%data%', education.schools[i].name);
+      var formattedSchoolDegree = HTMLschoolDegree.replace('%data%', education.schools[i].degree);
+      var formattedSchoolDates = HTMLschoolDates.replace('%data%', education.schools[i].dates);
+      var formattedSchoolLocation = HTMLschoolLocation.replace('%data%', education.schools[i].location);
+      var formattedSchoolMajor = HTMLschoolMajor.replace('%data%', education.schools[i].majors);
+
+      $('.education-entry:last').append(formattedSchoolName);
+      $('.education-entry:last').append(formattedSchoolDegree);
+      $('.education-entry:last').append(formattedSchoolDates);
+      $('.education-entry:last').append(formattedSchoolLocation);
+      $('.education-entry:last').append(formattedSchoolMajor);
+    }
+
+    $('#education').append(HTMLonlineClasses);
+
+    for (j = 0; j < education.onlineCourses.length; j++) {
+      $('#education').append(HTMLschoolStart);
+
+      var formattedOnlineTitle = HTMLonlineTitle.replace('%data%', education.onlineCourses[j].title);
+      var formattedOnlineSchool = HTMLonlineSchool.replace('%data%', education.onlineCourses[j].school);
+      var formattedOnlineCourse = formattedOnlineTitle + formattedOnlineSchool;
+      var formattedOnlineDates = HTMLonlineDates.replace('%data%', education.onlineCourses[j].dates);
+      var formattedOnlineURL = HTMLonlineURL.replace('%data%', education.onlineCourses[j].url);
+
+      $('.education-entry:last').append(formattedOnlineCourse);
+      $('.education-entry:last').append(formattedOnlineDates);
+      $('.education-entry:last').append(formattedOnlineURL);
+
+
+    }
+  }
+};
+
+education.display();
