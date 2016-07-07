0be02d1cc1132d15e09212bb65ca8cde807720a1
Kar Sheng Lee <karshenglee@Kars-MacBook-Air.local>
2016-07-07 17:35:30 +0800

    added footer contact section (moved contacts variables to global for header and footer access)

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,6 @@
 var formattedName = HTMLheaderName.replace('%data%','Karsheng Lee');
 var formattedRole = HTMLheaderRole.replace('%data%', 'Web Developer');
 
-
 $('#header').prepend(formattedRole);
 $('#header').prepend(formattedName);
 
@@ -18,31 +17,34 @@ var bio = {
   },
   'biopic': 'https://scontent-sin1-1.cdninstagram.com/t51.2885-15/e15/10616777_526199117481379_500143064_n.jpg?ig_cache_key=NzkyNzQ0NDU1NDg3ODY3MTEx.2',
   'welcomeMessage' : 'Hello I am Karsheng. I am a very good web developer!',
-  'skills' : skills,
-  'display' : function () {
-    var formattedMobile = HTMLmobile.replace('%data%', bio.contacts.mobile);
-    var formattedEmail = HTMLemail.replace('%data%', bio.contacts.email);
-    var formattedTwitter = HTMLtwitter.replace('%data%', bio.contacts.twitter);
-    var formattedGithub = HTMLgithub.replace('%data%', bio.contacts.github);
-    var formattedLocation = HTMLlocation.replace('%data%', bio.contacts.location);
-    var formattedbioPic = HTMLbioPic.replace('%data%', bio.biopic)
-    var formattedWelcomeMessage = HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage);
-
-    $('#topContacts').append(formattedMobile);
-    $('#topContacts').append(formattedEmail);
-    $('#topContacts').append(formattedGithub);
-    $('#topContacts').append(formattedTwitter);
-    $('#topContacts').append(formattedLocation);
-    $('#header').append(formattedbioPic);
-    $('#header').append(formattedWelcomeMessage);
-    $('#header').append(HTMLskillsStart);
-
-    for (i = 0; i < bio.skills.length; i++) {
-      var skill = HTMLskills.replace('%data%', bio.skills[i])
-      $('#skills-h3').append(skill);
-    }
-  }
+  'skills' : skills
 };
+
+var formattedMobile = HTMLmobile.replace('%data%', bio.contacts.mobile);
+var formattedEmail = HTMLemail.replace('%data%', bio.contacts.email);
+var formattedTwitter = HTMLtwitter.replace('%data%', bio.contacts.twitter);
+var formattedGithub = HTMLgithub.replace('%data%', bio.contacts.github);
+var formattedLocation = HTMLlocation.replace('%data%', bio.contacts.location);
+
+bio.display = function () {
+
+  var formattedbioPic = HTMLbioPic.replace('%data%', bio.biopic)
+  var formattedWelcomeMessage = HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage);
+
+  $('#topContacts').append(formattedMobile);
+  $('#topContacts').append(formattedEmail);
+  $('#topContacts').append(formattedGithub);
+  $('#topContacts').append(formattedTwitter);
+  $('#topContacts').append(formattedLocation);
+  $('#header').append(formattedbioPic);
+  $('#header').append(formattedWelcomeMessage);
+  $('#header').append(HTMLskillsStart);
+
+  for (i = 0; i < bio.skills.length; i++) {
+    var skill = HTMLskills.replace('%data%', bio.skills[i])
+    $('#skills-h3').append(skill);
+  }
+}
 
 bio.display();
 
@@ -194,3 +196,13 @@ var projects = {
 projects.display();
 
 $('#mapDiv').append(googleMap);
+
+function displayFooter() {
+  $('#footerContacts').append(formattedMobile);
+  $('#footerContacts').append(formattedEmail);
+  $('#footerContacts').append(formattedGithub);
+  $('#footerContacts').append(formattedTwitter);
+  $('#footerContacts').append(formattedLocation);
+}
+
+displayFooter();
